de908772288087b98a4e0a187903da3506907968
jdalmeida <37381332+jdalmeida@users.noreply.github.com>
2020-10-22 08:37:48 -0400

    Revert "tried to test with additional tube images"
    This reverts commit e679e789adaf42cb719a5626c1770af78dee9576.

--- analyze_image/analyzer.m
+++ analyze_image/analyzer.m
@@ -9,102 +9,85 @@ addpath('images');
 OD = 4;     % (mm) outer diameter
 Lc = 50;    % (mm) curved section length
 
-%% Read Image and Adjust
+%% Fit circle
 img = imread('cropped.jpg');        % load image
 [bw, rgb] = maskBlackTubes(img);    % mask out not black
-bw2 = bwareaopen(bw, 1000);
-bw3 = imfill(bw2, 'hole');           % 
-% figure
-% subplot(131)
-% imshow(bw)
-% subplot(132)
-% imshow(bw2)
-% subplot(133)
-% imshow(bw3)
+bw2 = imfill(bw, 'hole');
+bw3 = bwareaopen(bw2, 10);
 
-%% Get edge of the circle
-[y_bw, x__bw] = find(bw3);  % indicies of white points 
-y_bw = -y_bw;               % flip image for some reason
+% indicies of white points 
+[yi, xi] = find(bw3);
+yi = -yi;
 
-Y = [];
-X = [];
-delay = 0;     % to catch when bottom edge starts to curve up around the tip
+minYs = [];
+minXs = [];
+lasty = 0;
 
 % iterate through all possible indices of X
-% for i = min(x__bw)+delay:max(x__bw)
-for i = 10:2500
+for i = min(xi):2500
     % skip if i is not in x
-    if ~ismember(x__bw, i)
+    if ~ismember(xi, i)
         continue;
     end
     
-    % check if arc starts to curve back up and stop there
-    newy = min(y_bw(x__bw == i));
-%     if newy == 0
+    % check if arc starts to curve back up
+    newy = min(yi(xi == i));
+%     if newy > lasty
 %         continue;
 %     end
-%     if length(Y) > delay+1 && i > 1000
-%         % check curr y against previous ones with delay
-%         if abs(newy) < abs(Y(i-delay))
-%             break;
-%         end
-%     end
+    
+    lasty = newy;
     
     % store min y for inner side of tube
-    X(i) = i;
-    Y(i) = min(y_bw(x__bw == i));
+    minXs(i) = i;
+    minYs(i) = min(yi(xi == i));
 end
-
-%% ------ Calc R by first calculating bending angle
-% ** Assumes known length of bending section
-% [xc,yc,Re] = circle_fit(X,Y);
-% center = [xc, yc];      % coords for center of circle
-% [inner] = calcBendingAngle([X(1) Y(1)], [X(end) Y(end)], center);
-% unscaled_R = (inner/Lc) * 1e3
-
-
-%% ----- Calc R by scaling down
-% Assumes known OD and scaling factor is correct
-%   set the scale based on tube size by
-%   comparing width of img at base to known tube diameter
-
-x1 = min(x__bw);           % initial x's
-y1 = y_bw(x__bw == x1 );     % all initial y's
+% set the scale based on tube size
+%   set by comparing width of img at base to known tube diameter
+x1 = min(xi);           % initial x's
+y1 = yi(xi == x1 );     % all initial y's
 ydiff = abs(max(y1) - min(y1));
 scale = OD/ydiff;       % scale in mm/px
 
-% scale x and y
-X = X*scale;
-Y = Y*scale;
+x = minXs;
+y = minYs;
 
-[xc,yc,Re] = circle_fit(X,Y);
+[xc,yc,Re] = circle_fit(x,y);
 center = [xc, yc];      % coords for center of circle
+[inner] = calcBendingAngle([minXs(1) minYs(1)], [minXs(end) minYs(end)], center);
+unscaled_R = (inner/Lc) * 1e3
+
+x = x*scale;
+y = y*scale;
+
+[xc,yc,Re] = circle_fit(x,y);
+center = [xc, yc];      % coords for center of circle
+
 R = Re + OD/2;          % Radius of curvature at center is R at edge offset by tube radius
 
 % Calc bending angle of curve
-[inner_ang start_ang ustart utip] = calcBendingAngle([X(1) Y(1)], [X(end) Y(end)], center);
+[inner_ang start_ang ustart utip] = calcBendingAngle([x(1) y(1)], [x(end) y(end)], center);
 
 %% Plot results
 % create circle 
-% th = linspace(pi/2 - start_ang,pi/2 - inner_ang - start_ang,100)';
-th = linspace(0, 2*pi, 100);
+th = linspace(pi/2 - start_ang,pi/2 - inner_ang - start_ang,100)';
 xfit = Re*cos(th)+xc;
 yfit = Re*sin(th)+yc;
 
 x_real = R*cos(th)+xc; 
 y_real = R*sin(th)+yc;
 
-% plots
 figure
 hold on;
-% scatter(x__bw*scale,y_bw*scale, 'DisplayName', 'Mask of Tube')
-scatter(X,Y,'r', 'DisplayName', 'Fit Edge of Tube')
+scatter(xi*scale,yi*scale, 'DisplayName', 'Mask of Tube')
+scatter(x,y,'r', 'DisplayName', 'Fit Edge of Tube')
 plot(x_real,y_real,'-.', 'LineWidth', 1.5, 'DisplayName', 'Curve Fit of Tube');
 plot(xfit,yfit,'-.', 'LineWidth', 2, 'DisplayName', 'Curve Fit of Edge');
 quiver([center(1);center(1)],[center(2); center(2)], [ustart(1); utip(1)] * 10, [ustart(2); utip(2)] * 10,...
     'DisplayName', 'Bending Angle');
 
 title('Measured fitted and true circles')
+% text('Location', 'southwest',yc,sprintf('R=%g mm \nCurvatue=%g 1/m \nAngle=%g rad',R,1000/R, inner_ang))
 txt = sprintf('R= %.2fmm \nCurvatue= %.2f 1/m \nAngle= %.2fdeg',R,1000/R, rad2deg(inner_ang));
 annotation('textbox', [.6 0 .5 .3], 'String', txt, 'FitBoxToText', 'on')
 legend('Location', 'southwest')
