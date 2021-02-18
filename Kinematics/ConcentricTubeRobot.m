2f423472c001f99b2492b8c07443575a30f62304
jdalmeida <37381332+jdalmeida@users.noreply.github.com>
2021-02-17 21:21:03 -0500

    fixed inplane bending method

--- Kinematics/ConcentricTubeRobot.m
+++ Kinematics/ConcentricTubeRobot.m
@@ -13,6 +13,8 @@ classdef ConcentricTubeRobot < handle
         Ls      % nx1 vector containing the lengths of straight section of each tube
         E       % nx1 vector containing the Young moduli of each tube
         I       % nx1 vector containins the second moment of inertia for each tube
+        
+        q       % nx[translation rotation] current configuration
     end
     
     methods
@@ -38,19 +40,21 @@ classdef ConcentricTubeRobot < handle
             %   converts joint to arcs to send to each tube
             %INPUT
             %   q = [nTubes x [translation rotation]] joint variables
-            
+            self.q = q;
             numOverlaps = 2*self.nTubes;
             
             % Calculate overlap lengths
             % matrix defining curved/straight/skip for each tube at each overlap
             % isCurved = zeros(numOverlaps, numTubes);
-            [s isCurved] = calcLinkLengths(self.tubes, q(:,1));
+            [s, isCurved] = calcLinkLengths(self.tubes, q(:,1));
             
             % get emergent curvatures for each overlap section
             newK = zeros(numOverlaps, 1);  % init new ks for each overlapped section
             phi = zeros(numOverlaps, 1);
+            
             for  link = 1:numOverlaps
-                [newK(link), phi(link)] = inplane_bending(self.tubes, q(:,2), isCurved(link,:));
+%                 [newK(link), phi(link)] = inplane_bending(self.tubes,q(:,2), isCurved(link,:));
+                [newK(link), phi(link)] = self.inplane_bending(q(:,2), isCurved(link,:));
             end
             
             arcs = self.links2arcs(numOverlaps, newK, phi, isCurved, s);
@@ -61,7 +65,7 @@ classdef ConcentricTubeRobot < handle
         end
         
         %% -----HELPERS----
-        function [k, phi] = inplane_bending(self, theta, isCurved)
+        function [k_eq, phi_eq] = inplane_bending(self, theta, isCurved)
             %INPLANE_BENDING uses the elastic deformation formulas found in Webster2009
             %  to calculate the resulting curvature
             %  INPUTS
@@ -73,18 +77,28 @@ classdef ConcentricTubeRobot < handle
             %   phi =  rotation of link
             
             % array that modifies isCurved for if the tube is there
+            % if section is straight, still exists, but if -1 then does not
+            % exist
             secExists = isCurved;
+            secExists(secExists == 0) = 1;
             secExists(secExists == -1) = 0;
+            
+            % array that modifies isCurved for numerator
+            addCurvature = isCurved;
+            addCurvature(addCurvature == -1) = 0;
             
             M = self.E .* self.I;
             
             theta = reshape(theta, size(M));
             
-            kx = sum(M .* self.k .* cos(theta) .* isCurved) / sum(M * secExists);
-            ky = sum(M .* self.k .* sin(theta) .* isCurved) / sum(M * secExists);
+            kx =  sum(M .* self.k .* cos(theta) .* addCurvature) / sum(M .* secExists);
+            ky = sum(M .* self.k .* sin(theta) .* addCurvature) ./ sum(M .* secExists);
             
-            k = sqrt(kx.^2 + ky.^2);
-            phi = atan2(ky, kx);
+            if isnan(kx), kx = 0; end
+            if isnan(ky), ky = 0; end
+            
+            k_eq = sqrt(kx.^2 + ky.^2);
+            phi_eq = atan2(ky, kx);
         end
         
         function arcs = links2arcs(self, numOverlaps, newK, phi, isCurved, Ls)
@@ -114,6 +128,42 @@ classdef ConcentricTubeRobot < handle
                     arcs(link,:,t) = [k rel_phi s];
                 end
             end
+        end
+        
+        function psi = calc_torsional_flex(self, alpha)
+            options = optimoptions('fsolve', 'OptimalityTolerance', 1e-10);
+            dU = @self.energy_func;
+            
+            initials = self.q(:,2);
+            psi = fsolve(dU, initials, options);
+            
+        end
+        
+        function F = energy_func(self, x)
+            psi1 = x(1);
+            psi2 = x(2);
+            
+            l1 = self.q(1,1);
+            l2 = self.q(2,1);
+            
+            alpha1 = self.q(1,2);
+            alpha2 = self.q(2,2);
+            
+            % consts
+            J = 2 * self.I;
+            
+            v = .30; % .30 - .55
+            G = self.E/(2*(1+v));
+            
+            c1 = G(1) * J(1)/self.Ls(1);
+            c2 = G(2) * J(2)/self.Ls(2);
+            c3 = self.E(1)*self.I(1)*self.E(2)*self.I(2)*self.k(1)*self.k(2)/...
+                        (self.E(1)*self.I(1)+self.E(2)*self.I(2));
+            
+            f1 = c3*l2*sin(psi1 - psi2) - (c1*(2*alpha1 - 2*psi1))./2;
+            f2 = -(c2*(2*alpha2 - 2*psi2))./2 - c3*l2*sin(psi1 - psi2);
+
+            F = [f1; f2];
         end
         
         function plotTubes(self)
--- main.m
+++ main.m
@@ -5,20 +5,18 @@ addpath('kinematics');
 %% CREATE PRECURVED TUBES
 
 % parameters for generating tubes
-nTubes = 3;                     % number of tubes
-ODs = [3e-3 2e-3 1e-3];         % (m) outer diameters 
-IDs = ODs - 0.5-3;              % (m) inner diameters
+ODs = [3e-3 2e-3];         % (m) outer diameters 
+IDs = ODs - 0.5e-3;             % (m) inner diameters
 E = 1900e6;
 
-precurves = [15 40 55];
-Ls = [100e-3 100e-3 100e-3];                    % (m) length of straight section
-Lc = [50e-3 50e-3 50e-3];                     % (m) length of curved section
+precurves = [15 30];
+Ls = [100e-3 100e-3];                    % (m) length of straight section
+Lc = [50e-3 50e-3];                     % (m) length of curved section
 
 robot = ConcentricTubeRobot(ODs, IDs, precurves, Ls, Lc, E);
 
-q = [5e-3   deg2rad(0);        % outermost tube
-     20e-3  deg2rad(180);
-     30e-3 0];     % innermost tube
+q = [0e-3   deg2rad(0);        % outermost tube
+     20e-3  deg2rad(180)];     % innermost tube
  
 robot.fwkine(q);
 robot.plotTubes();
